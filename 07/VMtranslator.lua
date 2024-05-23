58b48aa7e5132aa3944a01e3e47d198dc602b27b
yasseen <ywr5999@rit.edu>
2024-05-23 15:45:52 -0400

    commit

--- 07/VMtranslator.lua
+++ 07/VMtranslator.lua
@@ -1,82 +1,188 @@
 local lfs = require "lfs"
-local bit = bit32 or require("bit")
 
-RAM_MAP = {
-    constant = 0,
-    argument = 2,
-    pointer = 3,
-    this = 3,
-    that = 4,
-    temp = 5
+Segments = {
+    ["argument"] = "ARG",
+    ["local"] = "LCL",
+    ["this"] = "THIS",
+    ["that"] = "THAT"
 }
 
+function ConvertSegementIndexIntoAddress()
+
+end
+
 function FileExists(name)
-    local f=io.open(name,"r")
-    if f~=nil then io.close(f) return true else return false end
- end
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else return false end
+end
 
-function ConvertPushInstruction(PushInstruction)
-    local words = {}
-    for word in PushInstruction:gmatch("%S+") do
-        table.insert(words, word)
-    end
-    
-    local segment = words[2]
-    local index = tonumber(words[3])
-    local address = (segment == "local") and 1 or RAM_MAP[segment]
-    local TheStringToReturn
-    print("Push Instruction is " .. PushInstruction)
-    print("\t" .. words[2])
-    print("\t" .. address .. "\n")
+function ConvertPushInstruction(instruction)
+    --[[
+        "push segment index"
+        Sitaution to store the data in the D register:
+            "push constant k"
+                    @k
+                    D=A
+            "push argument/local/this/that k"
+                K is 0:
+                    @ARG/LCL/THIS/THAT
+                    A=M
+                K is 1:
+                    @ARG/LCL/THIS/THAT
+                    A=M+1
+                K is 2:
+                    @ARG/LCL/THIS/THAT
+                    A=M+1
+                    A=A+1
+                Else:
+                    @ARG/LCL/THIS/THAT
+                    D=M
+                    @k
+                    A=D+A
+                    D=M
+                Always with D=M
+            "push pointer/temp k"
+                @(3/5 + k)
+                A=M
+                D=M
+    Always end with D=M except for pushing a constant
+    ]]
+    local command, segment, index = instruction:match("(%S+)%s+(%S+)%s+(%d+)")
+    local asm = "\n//Start push instruction (" .. instruction .. ")\n"
 
-    if segment == "constant" then
-        TheStringToReturn = "@" .. index .. "\nD=A\n"
-    else
-        TheStringToReturn = "@" .. address .. "\nA=M"
-        for i = 1, index do
-            TheStringToReturn = TheStringToReturn .. "\nA=A+1"
+    if command == "push" then
+        if segment == "constant" then
+            asm = asm .. "@" .. index .. "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elseif Segments[segment] then
+            local base = Segments[segment]
+            local offset = tonumber(index)
+            asm = asm .. "@" .. base .. "\n"
+            if offset == 0 then
+                asm = asm .. "A=M\nD=M\n"
+            elseif offset == 1 then
+                asm = asm .. "A=M+1\nD=M\n"
+            elseif offset == 2 then
+                asm = asm .. "A=M+1\nA=A+1\nD=M\n"
+            else
+                asm = asm .. "D=M\n@" .. index .. "\nA=D+A\nD=M\n"
+            end
+            asm = asm .. "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elseif segment == "pointer" then
+            asm = asm .. "@" .. (3 + tonumber(index)) .. "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elseif segment == "temp" then
+            asm = asm .. "@" .. (5 + tonumber(index)) .. "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
         end
-        TheStringToReturn = TheStringToReturn .. "\nD=M\n"
     end
-    
-    return TheStringToReturn .. "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    return asm
 end
 
 function ConvertPopInstruction(PopInstruction)
-    local words = {}
-    for word in PopInstruction:gmatch("%S+") do
-        table.insert(words, word)
+    --[[
+    "pop segment index"
+        Sitaution:
+            pop argument/local/this/that k for k < 8
+                @SP
+                M=M-1
+                A=M
+                D=M
+                M=0
+                @ARG/LCL/THIS/THAT
+                {
+                    A=M if 0
+
+                    A=M+1 if 1
+
+                    A=M+1
+                    A=A+1 if 2
+
+                    A=M+1
+                    A=M+1
+                    A=A+1 if 3
+
+                    ...
+                }
+                M=D
+            pop argument/local/this/that k for k>=8
+                @k
+                D=A
+                @ARG/LCL/THIS/THAT
+                D=D+M
+                @13
+                M=D
+                @SP
+                M=M-1
+                A=M
+                D=M
+                M=0
+                @13
+                A=M
+                M=D
+            "pop pointer/temp k"
+                @SP
+                M=M-1
+                A=M
+                D=M
+                M=0
+                @(3/5 + k) //calculate this
+                A=M
+                M=D
+    Always end with M=D
+    ]]
+
+    local command, segment, index = PopInstruction:match("(%S+)%s+(%S+)%s+(%d+)")
+    local asm = "\n//Start pop instruction (" .. PopInstruction .. ")\n"
+
+    if command == "pop" then
+        local popfromstack = "@SP\nM=M-1\nA=M\nD=M\nM=0\n//Go to the address\n@"
+        if Segments[segment] then
+            local base = Segments[segment]
+            local offset = tonumber(index)
+            if offset < 8 then
+                asm = asm .. popfromstack .. base .. "\n"
+                if offset == 0 then
+                    asm = asm .. "A=M\n"
+                elseif offset == 1 then
+                    asm = asm .. "A=M+1\n"
+                elseif offset == 2 then
+                    asm = asm .. "A=M+1\nA=A+1\n"
+                else
+                    asm = asm .. "A=M+1\n"
+                    for i = 2, offset do
+                        asm = asm .. "A=A+1\n"
+                    end
+                end
+            else
+                --By this point, store the address in the general purpose register for later
+                asm = asm .. "@" .. base .."\nD=M\n@" .. index .. "\nD=D+A\n@13\nM=D\n" .. popfromstack .. "13\nA=M\n"
+            end
+            asm = asm .. "M=D\n"
+        elseif segment == "pointer" then
+            asm = asm .. popfromstack .. (3 + tonumber(index)) .. "\nM=D\n"
+        elseif segment == "temp" then
+            asm = asm.. popfromstack .. (5 + tonumber(index)) .. "\nM=D\n"
+        end
     end
-    local segment = words[2]
-    local index = tonumber(words[3])
-    local address = (segment == "local") and 1 or RAM_MAP[segment]
-    print("Pop Instruction is " .. PopInstruction)
-    print("\t" .. words[2])
-    print("\t" .. address .. "\n")
-    local TheStringToReturn = "@SP\nA=M\nA=A-1\nD=M\nM=0\n@SP\nM=M-1\n@" .. address .. "\nA=M"
-    for i = 1, index do
-        TheStringToReturn = TheStringToReturn .. "\nA=A+1"
-    end
-    TheStringToReturn = TheStringToReturn .. "\nM=D\n"
-    
-    return TheStringToReturn
+    return asm
 end
 
 function DoMath(MathInstruction)
     --The VM represents true and false as -1 (minus one, 0xFFFF) and 0 (zero, 0x0000), respectively.
-    TheStringToReturn = ""
+    TheStringToReturn = "\n//Math: " .. MathInstruction .. "\n"
     --There are two types of MathInstructions, those with one input and those with two
-    TwoInputInstructions = {"sub", "add", "eg", "gt", "lt", "and", "or"}
-    OneInputInstructions = {"neg", "not"}
-        --Go to the stack pointer and store the first value in the D register. Then go to the next value
-        TheStringToReturn = "@SP\nA=M\nA=A-1\nA=A-1\nD=M\nA=A+1\n"
-        if MathInstruction == "sub" then
-            TheStringToReturn = TheStringToReturn .. "D=D-M\n"
-        end
-        if MathInstruction == "add" then
-            TheStringToReturn = TheStringToReturn .. "D=D+M\n"
-        end
-        TheStringToReturn = TheStringToReturn .. "M=0\nA=A-1\nM=D\n@SP\nM=M-1\n"
+    TwoInputInstructions = { "sub", "add", "eg", "gt", "lt", "and", "or" }
+    OneInputInstructions = { "neg", "not" }
+    --Go to the stack pointer and store the first value in the D register. Then go to the next value
+    TheStringToReturn = TheStringToReturn .. "@SP\nA=M-1\nA=A-1\nD=M\nA=A+1\n"
+    if MathInstruction == "sub" then
+        TheStringToReturn = TheStringToReturn .. "D=D-M\n"
+    end
+    if MathInstruction == "add" then
+        TheStringToReturn = TheStringToReturn .. "D=D+M\n"
+    end
+    TheStringToReturn = TheStringToReturn .. "M=0\nA=A-1\nM=D\n@SP\nM=M-1\n"
     return TheStringToReturn
 end
 
@@ -88,20 +194,23 @@ function CreateTheAsmFile(filename)
     local output_file = io.open(filename .. ".asm", 'w')
     for line in parsed:gmatch("[^\n]+") do
         if line:match("^push") then
+            print(line)
             -- ^@: Matches lines that start with '@' (A-instruction)
             output_file:write(ConvertPushInstruction(line))
         elseif line:match("^pop") then
+            print(line)
             -- ^%(: Excludes lines that start with '(' (labels)
             output_file:write(ConvertPopInstruction(line))
-            else 
-                if line:match("^add") or line:match("^sub") then
-                    output_file:write(DoMath(line))
-                end
+        else
+            if line:match("^add") or line:match("^sub") then
+                print(line)
+                output_file:write(DoMath(line))
             end
+        end
     end
     output_file:close()
-
 end
+
 -- First check if the input is a .vm or a path
 if string.sub(arg[1], -3) == ".vm" then
     if FileExists(arg[1]) then
@@ -114,7 +223,7 @@ else
         print("Directory doesn't exist")
     else
         --Now check if the file exists there
-        
+
         if FileExists(arg[1]:gsub("^.*/", "") .. ".vm") then
             --print(lfs.currentdir())
             --print(arg[1]:match("([^/]+)$") .. ".vm")
