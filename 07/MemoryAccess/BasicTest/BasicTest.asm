998fbeede50cc0357a6fb31a6ae57183b768ffea
Yasseen Ramadan <71387903+Ametuer@users.noreply.github.com>
2024-05-23 22:44:20 -0400

    most efficient possible asm. 73 HACK instructions total
    This is (what I think) is the most efficient possible HACK assembly code possible for BasicTest. Utilizes chunks of code regarding addition and subtraction where the addresses can be accessed without needing to overwrite the data register (acting like an accumulator)

--- 07/MemoryAccess/BasicTest/BasicTest.asm
+++ 07/MemoryAccess/BasicTest/BasicTest.asm
@@ -58,99 +58,39 @@ D=A
 M=D
 
 // Push local 0 (10) onto the stack
-@LCL
-A=M
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-
 // Push that 5 (42) onto the stack
+// Add top two stack values (local 0 + that 5) (+1 NET STACK)
 @THAT
 D=M
 @5
 A=D+A
 D=M
-@SP
-M=M+1
-A=M-1
-M=D
-
-
-// Add top two stack values (local 0 + that 5)
-@SP
-AM=M-1
-D=M
-A=A-1
-M=M+D
-
-// Push argument 1 (21) onto the stack
+@LCL
+A=M
+D=D+M
 @ARG
 A=M+1
-D=M
+D=D-M
 @SP
 M=M+1
 A=M-1
 M=D
 
-// Subtract argument 1 from (local 0 + that 5)
-@SP
-AM=M-1
-D=M
-A=A-1
-M=M-D
 
-// Push this 6 (36) onto the stack
+// Push this 6 (36) onto the stack twice,  add then subtract (NET ZERO STACK).
 @THIS
 D=M
 @6
 A=D+A
 D=M
+D=D+M
 @SP
-M=M+1
 A=M-1
-M=D
-
-
-// Push this 6 (36) again onto the stack
-@THIS
-D=M
-@6
-A=D+A
-D=M
-@SP
-M=M+1
-A=M-1
-M=D
-M=D+M
-
-// Add the top two stack values (this 6 + this 6)
-@SP
-AM=M-1
-D=M
-A=A-1
-M=M+D
-
-// Subtract the next two stack values ((local 0 + that 5 - argument 1) - (this 6 + this 6))
-@SP
-AM=M-1
-D=M
-A=A-1
 M=M-D
 
-// Push temp 6 (510) onto the stack
+// Push temp 6 (510) onto the stack then add (NET ZERO STACK)
 @11
 D=M
 @SP
-M=M+1
 A=M-1
-M=D
-
-
-// Add the final two stack values ((local 0 + that 5 - argument 1) - (this 6 + this 6) + temp 6)
-@SP
-AM=M-1
-D=M
-A=A-1
-M=M+D
+M=D+M
