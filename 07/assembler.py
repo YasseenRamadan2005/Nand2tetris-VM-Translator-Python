dcd5de17eb17c8e2570a333e243921c27d771c4d
Yasseen Ramadan <71387903+Ametuer@users.noreply.github.com>
2024-06-04 03:04:42 -0400

    Add files via upload
    Decided to do it in python

---
+++ 07/assembler.py
@@ -0,0 +1,315 @@
+# Assemble each instruction
+two_input_simple_ops = {"add", "sub", "and", "or"}
+comparison_ops = {"gt", "lt", "eq"}
+one_input_ops = {"neg", "not"}
+amount_of_return_calls_for_the_comparison_ops = 0
+
+segment_map = {
+    "argument": "ARG",
+    "local": "LCL",
+    "this": "THIS",
+    "that": "THAT",
+}
+
+
+def is_reachable(segment, index):
+    return (
+        segment in {"pointer", "temp"}
+        or segment == "constant"
+        or segment == "static"
+        or (segment in segment_map and int(index) < 8)
+    )
+
+
+def go_to_address(segment, index, name_of_file):
+    index = int(index)
+    if segment == "constant" and index not in {-1, 0, 1}:
+        return f"@{index}\n"
+    if segment == "static":
+        return f"@{name_of_file}.{index}\n"
+    if segment in {"pointer", "temp"}:
+        base_address = 3 if segment == "pointer" else 5
+        return f"@{base_address + index}\n"
+    if segment in segment_map:
+        base_segment = segment_map[segment]
+        if index == 0:
+            return f"@{base_segment}\nA=M\n"
+        elif index < 8:
+            return f"@{base_segment}\nA=M+1\n" + "A=A+1\n" * (index - 1)
+    return f"@{index}\nD=A\n@{segment_map[segment]}\nA=D+M\n"
+
+
+def sole_push_instruction(instruction, name_of_file):
+    _, segment, index = instruction.split()
+    go_to_top_of_stack = "@SP\nM=M+1\nA=M-1\n"
+
+    # Load constant value onto stack
+    if segment == "constant":
+        if index == "0":
+            return go_to_top_of_stack + "M=0\n"
+        elif index == "1":
+            return go_to_top_of_stack + "M=1\n"
+        return f"@{index}\nD=A\n{go_to_top_of_stack}M=D\n"
+
+    # Load value from segment onto stack
+    address_code = go_to_address(segment, index, name_of_file)
+    return (
+        "//"
+        + instruction
+        + "\n"
+        + address_code
+        + "D=M\n"
+        + go_to_top_of_stack
+        + "M=D\n"
+    )
+
+
+def sole_pop_instruction(instruction, name_of_file):
+    _, segment, index = instruction.split()
+    decrement_stack_pointer = "@SP\nAM=M-1\nD=M\n"
+
+    # Store value from stack into segment
+    if segment in {"pointer", "temp"}:
+        base_address = 3 if segment == "pointer" else 5
+        return f"{decrement_stack_pointer}@{base_address + int(index)}\nM=D\n"
+
+    if segment == "static":
+        return f"{decrement_stack_pointer}@{name_of_file}.{index}\nM=D\n"
+
+    address_code = go_to_address(segment, index, name_of_file)
+    if int(index) < 8:
+        return f"{decrement_stack_pointer}{address_code}M=D\n"
+
+    return (
+        f"{decrement_stack_pointer}"
+        f"@{index}\nD=A\n@{segment_map[segment]}\nD=D+M\n@R13\nM=D\n"
+        f"@R13\nA=M\nM=D\n"
+    )
+
+
+def push_pop_parser(instructions, name_of_file):
+    the_amount_of_lines = len(instructions)
+    the_string_to_return = ""
+    for i in range(the_amount_of_lines // 2):
+        the_string_to_return += push_pop_optimized(
+            instructions[i], instructions[the_amount_of_lines - 1 - i], name_of_file
+        )
+    return the_string_to_return
+
+
+def push_pop_optimized(push_instruction, pop_instruction, name_of_file):
+    _, push_segment, push_index = push_instruction.split()
+    _, pop_segment, pop_index = pop_instruction.split()
+    optimized_code = "//" + push_instruction + " , " + pop_instruction + "\n"
+
+    # Load value from push segment
+    if push_segment == "constant":
+        optimized_code += f"@{push_index}\nD=A\n"
+    else:
+        optimized_code += (
+            go_to_address(push_segment, push_index, name_of_file) + "D=M\n"
+        )
+
+    # Store value into pop segment
+    if pop_segment in {"pointer", "temp"}:
+        base_address = 3 if pop_segment == "pointer" else 5
+        optimized_code += f"@{base_address + int(pop_index)}\nM=D\n"
+    elif pop_segment == "static":
+        optimized_code += f"@{name_of_file}.{pop_index}\nM=D\n"
+    else:
+        if int(pop_index) < 8:
+            optimized_code += (
+                go_to_address(pop_segment, pop_index, name_of_file) + "M=D\n"
+            )
+        else:
+            optimized_code += (
+                f"@R13\nM=D\n"
+                f"@{pop_index}\nD=A\n@{segment_map[pop_segment]}\nD=D+M\n@R14\nM=D\n"
+                f"@R13\nD=M\n@R14\nA=M\nM=D\n"
+            )
+
+    return optimized_code
+
+
+def convert_sole_math_instr(math_instruction):
+    global amount_of_return_calls_for_the_comparison_ops
+    the_string_to_return = ""
+
+    if math_instruction in two_input_simple_ops:
+        the_string_to_return = "@SP\nAM=M-1\nD=M\nA=A-1\n"
+        op_map = {"add": "M=D+M\n", "sub": "M=M-D\n", "and": "M=D&M\n", "or": "M=D|M\n"}
+        the_string_to_return += op_map[math_instruction]
+        return the_string_to_return
+
+    if math_instruction in comparison_ops:
+        comp_map = {"lt": "M=-1\n", "eq": "M=0\n", "gt": "M=1\n"}
+        return_label = (
+            f"RETURN.FROM.COMP.OPS.{amount_of_return_calls_for_the_comparison_ops}"
+        )
+        the_string_to_return = (
+            f"@{return_label}\nD=A\n@14\n"
+            f"{comp_map[math_instruction]}"
+            "@COMP_BEGIN\n0;JMP\n"
+            f"({return_label})\n"
+        )
+        amount_of_return_calls_for_the_comparison_ops += 1
+        return the_string_to_return
+
+    the_string_to_return = "@SP\nA=M-1\n"
+    if math_instruction == "neg":
+        the_string_to_return += "M=-M\n"
+    elif math_instruction == "not":
+        the_string_to_return += "M=!M\n"
+
+    return the_string_to_return
+
+
+def convert_double_push_math_group(instructions, name_of_file):
+    push1, push2, math_instruction = instructions
+    _, segment1, index1 = push1.split()
+    _, segment2, index2 = push2.split()
+
+    math_op_map = {
+        "add": "D=D+M",
+        "sub": "D=M-D",
+        "and": "D=D&M",
+        "or": "D=D|M",
+    }
+
+    A_math_op_map = {
+        "add": "D=D+A",
+        "sub": "D=A-D",
+        "and": "D=D&A",
+        "or": "D=D|A",
+    }
+
+    address1 = go_to_address(segment1, index1, name_of_file)
+    address2 = go_to_address(segment2, index2, name_of_file)
+
+    def load_value(segment, address):
+        if segment == "constant":
+            return address + "D=A\n"  # Load constant value into D
+        else:
+            return address + "D=M\n"  # Load value from memory into D
+
+    comment = f'// "{push1}, {push2}, {math_instruction}"\n'
+    result_push = "@SP\nAM=M+1\nA=A-1\nM=D  // Push the result to the stack\n"
+
+    # Handle the case where the same address is used twice
+    if segment1 == segment2 and index1 == index2:
+        return (
+            comment
+            + address1
+            + "D=M\n"  # Load value from memory into D
+            + math_op_map[math_instruction]
+            + "\n"
+            + result_push
+        )
+
+    # Handle both reachable segments
+    if is_reachable(segment1, index1) and is_reachable(segment2, index2):
+        return (
+            comment
+            + load_value(segment2, address2)
+            + address1
+            + (
+                A_math_op_map[math_instruction]
+                if segment1 == "constant"
+                else math_op_map[math_instruction]
+            )
+            + "\n"
+            + result_push
+        )
+
+    # Handle only one reachable segment
+    if is_reachable(segment1, index1) or is_reachable(segment2, index2):
+        (
+            reachable_segment,
+            unreachable_segment,
+            reachable_address,
+            unreachable_address,
+        ) = (
+            (segment1, segment2, address1, address2)
+            if is_reachable(segment1, index1)
+            else (segment2, segment1, address2, address1)
+        )
+        return (
+            comment
+            + unreachable_address
+            + "D=M\n"
+            + load_value(reachable_segment, reachable_address)
+            + (
+                A_math_op_map[math_instruction]
+                if unreachable_segment == "constant"
+                else math_op_map[math_instruction]
+            )
+            + "\n"
+            + result_push
+        )
+
+    # Both addresses are unreachable
+    return (
+        comment
+        + load_value(segment1, address1)
+        + "@13\nM=D  // Store the first value in R13\n"
+        + address2
+        + "D=M\n@13\n"
+        + math_op_map[math_instruction]
+        + "\n"
+        + result_push
+    )
+
+
+def convert_push_math_group(instructions, name_of_file):
+    push_instruction, math_instruction = instructions
+    _, segment, index = push_instruction.split()
+
+    # Define the mapping for simple arithmetic/logical operations
+    math_op_map = {
+        "add": "D+M",
+        "sub": "M-D",
+        "and": "D&M",
+        "or": "D|M",
+    }
+
+    # Define the mapping for unary operations
+    unary_op_map = {
+        "neg": "-M",
+        "not": "!M",
+    }
+
+    address = go_to_address(segment, index, name_of_file)
+
+    def load_value(segment, address):
+        if segment == "constant":
+            return address + "D=A\n"  # Load constant value into D
+        else:
+            return address + "D=M\n"  # Load value from memory into D
+
+    comment = f'// "{push_instruction}, {math_instruction}"\n'
+    result_push = "@SP\nA=M-1\n"
+
+    # Handle the push followed by a two-input math operation
+    if math_instruction in math_op_map:
+        return (
+            comment
+            + load_value(segment, address)
+            + result_push
+            + "M="
+            + math_op_map[math_instruction]
+            + "\n"
+        )
+
+    # Handle the push followed by a unary math operation
+    if math_instruction in unary_op_map:
+        return (
+            comment
+            + load_value(segment, address)
+            + unary_op_map[math_instruction]
+            + "\n"
+            + result_push
+            + "M=D\n"
+        )
+
+    # If the math instruction is not recognized
+    return f"// Unsupported instruction: {math_instruction}\n"
---
+++ 07/translator.py
@@ -0,0 +1,172 @@
+import os
+import sys
+from assembler import (
+    sole_push_instruction,
+    sole_pop_instruction,
+    convert_sole_math_instr,
+    convert_double_push_math_group,
+    convert_push_math_group,
+    push_pop_parser,
+    push_pop_optimized,
+)
+
+# Define sets of different types of operations
+simple_two_input_ops = {"add", "sub", "and", "or"}
+comp_ops = {"gt", "lt", "eq"}
+one_input_ops = {"neg", "not"}
+
+
+def count_consecutive_ops(lines, start_index, op):
+    """Counts consecutive occurrences of a given operation starting from a specific index."""
+    count = 0
+    while (
+        start_index + count < len(lines) and lines[start_index + count].split()[0] == op
+    ):
+        count += 1
+    return count
+
+
+def remove_comments(lines):
+    """Removes comments from each line in the given list of lines."""
+    cleaned_lines = []
+    for line in lines:
+        # Remove everything after "//" and strip any leading/trailing whitespace
+        cleaned_line = line.split("//")[0].strip()
+        if cleaned_line:  # Only add non-empty lines
+            cleaned_lines.append(cleaned_line)
+    return cleaned_lines
+
+
+def grouper_and_translate(instructions, name_of_file):
+    """Groups and translates VM instructions to assembly code."""
+    asm_code = ""
+    if "eq" in instructions or "lt" in instructions or "gt" in instructions:
+        # Add assembly code for comparison operations
+        asm_code += "@SKIP\n0;JMP\n(COMP_BEGIN)\n@13\nM=D //Store the return address @13\n@SP\nA=M-1\nD=M\nA=A-1\nD=M-D //Calculate the difference\n\n@15\nM=D\n@14 //Check the flag @14\nD=M\n@EQ_BEGIN\nD;JEQ\n@LT_BEGIN\nD;JLT\n\n//By this case, this is the greater than subroutine\n@15\nD=M\n@RETURN_TRUE\nD;JGT\n@RETURN_FALSE\n0;JMP\n\n(EQ_BEGIN)\n@15\nD=M\n@RETURN_TRUE\nD;JEQ\n@RETURN_FALSE\n0;JMP\n\n(LT_BEGIN)\n@15\nD=M\n@RETURN_TRUE\nD;JLT\n@RETURN_FALSE\n0;JMP\n\n(RETURN_TRUE)\nD=-1;\n@COMPLETE\n0;JMP\n\n(RETURN_FALSE)\nD=0;\n@COMPLETE\n0;JMP\n\n(COMPLETE)\n@SP\nAM=M-1\nA=A-1\nM=D\n@13\nA=M\n0;JMP\n(SKIP)\n\n"
+
+    lines = instructions.splitlines()
+    lines = remove_comments(lines)  # Remove comments from lines
+    index = 0
+    while index < len(lines):
+        current_line = lines[index]
+        parts = current_line.split()
+        operation = parts[0]
+
+        if operation == "push":
+            push_count = count_consecutive_ops(lines, index, "push")
+            if index + push_count < len(lines):
+                next_op = lines[index + push_count].split()[0]
+            else:
+                next_op = None
+            if next_op == "pop":
+                # If consecutive push and pop operations, parse them together
+                pop_count = count_consecutive_ops(lines, index + push_count, "pop")
+                if push_count == pop_count:
+                    asm_code += push_pop_parser(
+                        lines[index : index + push_count + pop_count], name_of_file
+                    )
+                else:
+                    # If not equal number of push and pop operations, parse individually
+                    for i in range(push_count - pop_count):
+                        asm_code += (
+                            sole_push_instruction(lines[index + i], name_of_file) + "\n"
+                        )
+                    asm_code += (
+                        push_pop_parser(
+                            lines[
+                                index
+                                + push_count
+                                - pop_count : index
+                                + push_count
+                                + pop_count
+                            ],
+                            name_of_file,
+                        )
+                        + "\n"
+                    )
+                index += push_count + pop_count
+            elif next_op in simple_two_input_ops:
+                if push_count == 2:  # Check if it's a double push math group
+                    asm_code += (
+                        convert_double_push_math_group(
+                            lines[index : index + push_count + 1], name_of_file
+                        )
+                        + "\n"
+                    )
+                elif push_count == 1:  # It's a single push math group
+                    asm_code += (
+                        convert_push_math_group(
+                            lines[index : index + push_count + 1], name_of_file
+                        )
+                        + "\n"
+                    )
+                else:
+                    # Otherwise, parse individually
+                    for i in range(push_count - 2):
+                        asm_code += sole_push_instruction(
+                            lines[index + i], name_of_file
+                        )
+                    asm_code += convert_double_push_math_group(
+                        lines[index + push_count - 2 : index + push_count + 1],
+                        name_of_file,
+                    )
+                index += push_count + 1
+            else:
+                # Parse push instructions individually
+                for i in range(push_count):
+                    asm_code += (
+                        sole_push_instruction(lines[index + i], name_of_file) + "\n"
+                    )
+                index += push_count
+        else:
+            if operation in {"pop"}:
+                # Parse pop instruction
+                asm_code += sole_pop_instruction(current_line, name_of_file) + "\n"
+            elif (
+                operation in simple_two_input_ops
+                or operation in one_input_ops
+                or operation in comp_ops
+            ):
+                # Parse math instructions
+                asm_code += convert_sole_math_instr(operation) + "\n"
+            index += 1
+    return asm_code
+
+
+def translate_file(vm_file_path):
+    """Translates a single .vm file to .asm."""
+    name_of_file = os.path.splitext(os.path.basename(vm_file_path))[0]
+    asm_file_path = os.path.splitext(vm_file_path)[0] + ".asm"
+    with open(vm_file_path, "r") as vm_file:
+        vm_instructions = vm_file.read()
+    asm_code = grouper_and_translate(vm_instructions, name_of_file)
+    print(asm_code)
+    with open(asm_file_path, "w") as asm_file:
+        asm_file.write(asm_code)
+
+
+def main():
+    """Main function to handle file or directory input and translate .vm files to .asm."""
+    if len(sys.argv) != 2:
+        print("Usage: python translator.py <inputfile.vm | inputdirectory>")
+        sys.exit(1)
+
+    input_path = sys.argv[1]
+    if os.path.isdir(input_path):
+        # If input is a directory, translate all .vm files in the directory
+        for file_name in os.listdir(input_path):
+            if file_name.endswith(".vm"):
+                translate_file(os.path.join(input_path, file_name))
+    elif os.path.isfile(input_path) and input_path.endswith(".vm"):
+        # If input is a single .vm file, translate it
+        translate_file(input_path)
+    else:
+        # Invalid input path
+        print(
+            "Error: Invalid input path. Provide a .vm file or a directory containing .vm files."
+        )
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
